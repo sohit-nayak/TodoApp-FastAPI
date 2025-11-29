bf381e0d6d39aea88111b2b76e9354fb41348709
Sohit <sohit.nayak@gmail.com>
2025-11-29 12:37:58 -0500

    changed DB settings

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost/TodoAppDB"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp_deployment.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
